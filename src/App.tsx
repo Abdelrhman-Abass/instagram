2f1950a4594792bac85ef1d8ee9029ca45aed47e
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-01-24 15:37:45 +0200

    add routing and folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import { Routes , Route } from 'react-router-dom';
+
+
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+
+import { Home } from './_root/pages';
+
 import './index.css';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+
+
 const App = () => {
   return (
-    <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <main className='flex h-screen'>  
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout/>}>
+          <Route path="sign-in" element={<SigninForm />} />
+          <Route path="sign-in" element={<SignupForm />} />
+        </Route>
+
+
+        {/* private routes */}
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home/>}/>
+        </Route>
+
+        
+      </Routes>
+    </main>
   )
 }
 
